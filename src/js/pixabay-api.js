d5fa0a968902db4ed1e7360f69c61199bc0cd28d
Oksana-Horbachevska <oksanapaolo@gmail.com>
2025-05-24 17:46:19 +0200

    hw-11 axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,9 +2,35 @@ import axios from 'axios';
 
 // ===============функція js-fetch запиту =================
 
-export function getImagesByQuery(query) {
+// export function getImagesByQuery(query) {
+//   const BASE_URL = 'https://pixabay.com/';
+//   const END_POINTS = 'api/';
+//   const params = new URLSearchParams({
+//     key: '50383604-c913cf66ede56daf64b093a7e',
+//     q: query,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: 'true',
+//   });
+
+//   const url = `${BASE_URL}${END_POINTS}?${params}`;
+
+//   return fetch(url).then(res => {
+//     if (res.ok) {
+//       return res.json();
+//     } else {
+//       throw new Error('Something vent wrong...Please try again');
+//     }
+//   });
+// }
+
+// ===============функція js-axios запиту =================
+
+export async function getImagesByQuery(query) {
   const BASE_URL = 'https://pixabay.com/';
   const END_POINTS = 'api/';
+  const url = BASE_URL + END_POINTS;
+
   const params = new URLSearchParams({
     key: '50383604-c913cf66ede56daf64b093a7e',
     q: query,
@@ -13,33 +39,7 @@ export function getImagesByQuery(query) {
     safesearch: 'true',
   });
 
-  const url = `${BASE_URL}${END_POINTS}?${params}`;
+  const res = await axios.get(url, { params });
 
-  return fetch(url).then(res => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error('Something vent wrong...Please try again');
-    }
-  });
+  return res.data;
 }
-
-// ===============функція js-axios запиту =================
-
-// async function getImagesByQuery(query) {
-//   const baseUrl = '';
-//   const endPoints = '';
-//   const url = baseUrl + endPoints;
-// }
-// const params = new URLSearchParams({
-//   param1: '1',
-//   param2: '2',
-//   param3: '3',
-// });
-
-// const headers = {
-//   key1: 'value',
-// };
-
-// const res = await axios.get(url, { headers, params });
-// return res.data;
